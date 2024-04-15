ef1d4dff39abef1d4dd868be8b9a33fb9035d755
roudbib <rayan.oudbib@etud.univ-jfc.fr>
2024-04-15 21:39:10 +0200

    TP fini

--- pages/xstatepolyline/xStatePolyline.js
+++ pages/xstatepolyline/xStatePolyline.js
@@ -1,3 +1,4 @@
+
 import Konva from "konva";
 import { createMachine, interpret } from "xstate";
 
@@ -8,51 +9,90 @@ const stage = new Konva.Stage({
     height: 400,
 });
 
-const layer = new Konva.Layer();
-stage.add(layer);
+// Une couche pour le dessin
+const dessin = new Konva.Layer();
+// Une couche pour la polyline en cours de construction
+const temporaire = new Konva.Layer();
+stage.add(dessin);
+stage.add(temporaire);
 
 const MAX_POINTS = 10;
 let polyline // La polyline en cours de construction;
 
 const polylineMachine = createMachine(
     {
-        /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAcsAbATwBkBLDMfEI2SgF0qwzoA9EBaANnVI9eAOgAM4iZMkB2ZGnokK1MMMoRitJAsYs2nRABYATAMQAOAIzCD0gJwXetgwGZezgw9ty5QA */
+        /** @xstate-layout N4IgpgJg5mDOIC5QAcD2AbAngGQJYDswA6XCdMAYgFkB5AVQGUBRAYWwEkWBpAbQAYAuohSpYuAC65U+YSAAeiAIwAWAGxEArAGYAHDo0AmA3x2qNqxRoA0ITEoCciosoDsRg-Zf6DWg4pcAvgE2aFh4hETSYAAKqATi1PTMbJy8grJoYpLSsgoIFvZEOpbKij5l9jou9jZ2CIqOzm4+Ospa1YZ8qkEhGDgExFGx8YmMTLQAakz8QkggmRJSMnN5rlpE9ho69j5aWpVtLta2SnxOBjpaDRcWygZqOj3zfeGDhMP4CUywAMYAhsgwDMMqJFjkVkpVAZnNt2mV9tVfFpaohfHwigYNG0NOZVEcdAYnqF+hEALZ-fCYD7iWCjZIcbjAuYLbLLUB5RR8FzrcxaZR8YwXFyuFEIAxeIgubmqPH2eyuExtIkvAZEcmU6m02hjFKMxSzERZJa5U6qHSadp85RYy6KKqirR45xaPhdUqKO1mZTKsKq9VUuKfLVJcY0KZMw1gtnyJRlDREGVQtyGLHWlyiznnW07Uwqe6BYLPX1kikB+K077-QER55G8HsxA6AWaLmeHFVLFQh2FKoGKHy-ZaDRlAu9YvEf2aihMT5gABONZZxohCB2PalRiHpj5JlFAFpOc4PUd2splJcXDLHoXia81aWpwAhP4-ADWsGQL6B6WZoNZJoQFxFHUNp2kuGVrTxZQHWMDEsSHXF8UJG8VRLDVAxpChnzfD8vx4fUQTraNVlUZQikqTo7XsLlqhg9ECXgnEZXxRQtCCQt8FQCA4BBcdCKjAC9zUI9pVUAUvDRZETgQPdPHIvhHC8M12guZCxxJYhSHIfj-xXJwtj7PZPD8WUzR0UU-DIzFTE8UoqmAvkfQ0yJ3gwnTlwbBBTyIfZzz4DRqIJPgrgslQiGs1RbLtIDVEclDx3vdDy3c+sY1XS5JQ0BT2li6zzOkyyE0RD1gv2Kp5XYgIgA */
         id: "polyLine",
         initial: "idle",
-        states : {
+        states: {
             idle: {
                 on: {
                     MOUSECLICK: {
+                        target: "onePoint",
                         actions: "createLine",
-                        target: "drawing",
-                        cond: "pasPlein"
-                    }
-                }
+                    },
+                },
             },
-            drawing: {
+            onePoint: {
                 on: {
+                    MOUSECLICK: {
+                        target: "manyPoints",
+                        actions: "addPoint",
+                    },
                     MOUSEMOVE: {
-                        actions: "setLastPoint"
+                        actions: "setLastPoint",
                     },
-                    Backspace: {
-                        actions: "removeLastPoint",
-                        cond: "plusDeDeuxPoints"
+                    Escape: { // event.key
+                        target: "idle",
+                        actions: "abandon",
                     },
-                    Enter: {
-                        actions: "saveLine",
-                        cond: "nombrePointsValide"
+                },
+            },
+            manyPoints: {
+                on: {
+                    MOUSECLICK: [
+                        {
+                            actions: "addPoint",
+                            cond: "pasPlein",
+                        },
+                        {
+                            target: "idle",
+                            actions: ["addPoint", "saveLine"],
+                        },
+                    ],
+
+                    MOUSEMOVE: {
+                        actions: "setLastPoint",
                     },
+
                     Escape: {
-                        actions: "abandon"
-                    }
-                }
-            }
-        }
+                        target: "idle",
+                        actions: "abandon",
+                    },
+
+                    Enter: { // event.key
+                        target: "idle",
+                        actions: "saveLine",
+                    },
+
+                    Backspace: [ // event.key
+                        {
+                            target: "manyPoints",
+                            actions: "removeLastPoint",
+                            cond: "plusDeDeuxPoints",
+                            internal: true,
+                        },
+                        {
+                            target: "onePoint",
+                            actions: "removeLastPoint",
+                        },
+                    ],
+                },
+            },
+        },
     },
-    // Quelques actions et guardes que vous pouvez utiliser dans votre machine
     {
         actions: {
-            // Créer une nouvelle polyline
             createLine: (context, event) => {
                 const pos = stage.getPointerPosition();
                 polyline = new Konva.Line({
@@ -60,9 +100,8 @@ const polylineMachine = createMachine(
                     stroke: "red",
                     strokeWidth: 2,
                 });
-                layer.add(polyline);
+                temporaire.add(polyline);
             },
-            // Mettre à jour le dernier point (provisoire) de la polyline
             setLastPoint: (context, event) => {
                 const pos = stage.getPointerPosition();
                 const currentPoints = polyline.points(); // Get the current points of the line
@@ -70,64 +109,57 @@ const polylineMachine = createMachine(
 
                 const newPoints = currentPoints.slice(0, size - 2); // Remove the last point
                 polyline.points(newPoints.concat([pos.x, pos.y]));
-                layer.batchDraw();
+                temporaire.batchDraw();
             },
-            // Enregistrer la polyline
             saveLine: (context, event) => {
+                polyline.remove(); // On l'enlève de la couche temporaire
                 const currentPoints = polyline.points(); // Get the current points of the line
                 const size = currentPoints.length;
                 // Le dernier point(provisoire) ne fait pas partie de la polyline
                 const newPoints = currentPoints.slice(0, size - 2);
                 polyline.points(newPoints);
-                layer.batchDraw();
+                polyline.stroke("black"); // On change la couleur
+                // On sauvegarde la polyline dans la couche de dessin
+                dessin.add(polyline); // On l'ajoute à la couche de dessin
             },
-            // Ajouter un point à la polyline
             addPoint: (context, event) => {
                 const pos = stage.getPointerPosition();
                 const currentPoints = polyline.points(); // Get the current points of the line
                 const newPoints = [...currentPoints, pos.x, pos.y]; // Add the new point to the array
                 polyline.points(newPoints); // Set the updated points to the line
-                layer.batchDraw(); // Redraw the layer to reflect the changes
+                temporaire.batchDraw(); // Redraw the layer to reflect the changes
             },
-            // Abandonner le tracé de la polyline
             abandon: (context, event) => {
                 polyline.remove();
             },
-            // Supprimer le dernier point de la polyline
             removeLastPoint: (context, event) => {
                 const currentPoints = polyline.points(); // Get the current points of the line
                 const size = currentPoints.length;
                 const provisoire = currentPoints.slice(size - 2, size); // Le point provisoire
                 const oldPoints = currentPoints.slice(0, size - 4); // On enlève le dernier point enregistré
                 polyline.points(oldPoints.concat(provisoire)); // Set the updated points to the line
-                layer.batchDraw(); // Redraw the layer to reflect the changes
+                temporaire.batchDraw(); // Redraw the layer to reflect the changes
             },
         },
         guards: {
-            // On peut encore ajouter un point
             pasPlein: (context, event) => {
+                // On peut encore ajouter un point
                 return polyline.points().length < MAX_POINTS * 2;
             },
-            // On peut enlever un point
             plusDeDeuxPoints: (context, event) => {
                 // Deux coordonnées pour chaque point, plus le point provisoire
                 return polyline.points().length > 6;
             },
-            nombrePointsValide: (context, event) => {
-                const numPoints = polyline.points().length / 2;
-                return numPoints >= 2 && numPoints <= MAX_POINTS;
-            }
         },
     }
 );
 
-// On démarre la machine
 const polylineService = interpret(polylineMachine)
     .onTransition((state) => {
         console.log("Current state:", state.value);
     })
     .start();
-// On envoie les événements à la machine
+
 stage.on("click", () => {
     polylineService.send("MOUSECLICK");
 });
@@ -136,10 +168,7 @@ stage.on("mousemove", () => {
     polylineService.send("MOUSEMOVE");
 });
 
-// Envoi des touches clavier à la machine
 window.addEventListener("keydown", (event) => {
-    if (event.key === "Backspace" || event.key === "Enter" || event.key === "Escape") {
-        event.preventDefault(); // Prevent browser default actions for these keys
-        polylineService.send(event.key);
-    }
+    console.log("Key pressed:", event.key);
+    polylineService.send(event.key);
 });
